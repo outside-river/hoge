0db546da858f113d4dc592086c9434dbd7cf57b3
outside-river <106018384+outside-river@users.noreply.github.com>
2022-07-12 15:55:29 +0900

    Revert "uwaaa"
    This reverts commit 7165bd3108f3731c452e45c20e48976c5b113486.

--- Assets/hoge.cs
+++ Assets/hoge.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class hoge : MonoBehaviour
 {
-    //
-    //
-    //‚¤‚í‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ 
-    //
-    //
     // Start is called before the first frame update
     void Start()
     {
