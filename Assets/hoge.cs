7165bd3108f3731c452e45c20e48976c5b113486
outside-river <106018384+outside-river@users.noreply.github.com>
2022-07-12 15:45:41 +0900

    uwaaa

--- Assets/hoge.cs
+++ Assets/hoge.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class hoge : MonoBehaviour
 {
+    //
+    //
+    //‚¤‚í‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ 
+    //
+    //
     // Start is called before the first frame update
     void Start()
     {
